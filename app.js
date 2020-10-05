9fd189bb43b92f050e3cdec5544872186b4cee97
Yosra <yos.skhiri@gmail.com>
2020-10-05 21:47:50 +0100

    added the appropriate code to connect to the database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 require('dotenv').config();
@@ -8,6 +9,14 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('mongo DB success');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
