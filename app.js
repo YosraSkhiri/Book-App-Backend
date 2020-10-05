af790dc60ae9397d5cea3075186293b88a632af5
Yosra <yos.skhiri@gmail.com>
2020-10-05 21:34:28 +0100

    Modified the app.js file: I added cors (Cross-Origin Resource Sharing), added dotenv (a module that helps access env variables) and added the middleware express.json() to recognize the incoming requests as JSON Objects.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+require('dotenv').config();
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
