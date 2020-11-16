4ebea265e13090819850998020da48da32518d01
Yosra <yos.skhiri@gmail.com>
2020-11-16 14:06:41 +0100

    the data sent from the frontend is being handled and saved in the server and the database

--- routes/authors.js
+++ routes/authors.js
@@ -1,13 +1,38 @@
 const router = require('express').Router();
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+let path = require('path');
 let Author = require('../models/author.modal');
 
-router.route('/add').post((req, res) => {
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function(req, file, cb) {   
+        cb(null, uuidv4() + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    const allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+    if(allowedFileTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+let upload = multer({ storage, fileFilter });
+
+router.route('/add').post(upload.single('photo'), (req, res) => {
     const name = req.body.name;
     const birthdate = req.body.birthdate;
+    const photo = req.file.filename;
 
     const newAuthorData = {
         name,
-        birthdate
+        birthdate,
+        photo
     }
 
     const newAuthor = new Author(newAuthorData);
