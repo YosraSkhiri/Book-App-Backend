479e06b8beb4bcf05cfa7e96232d78956b7f2b75
Yosra <yos.skhiri@gmail.com>
2020-11-30 18:18:31 +0100

    the login works!

--- routes/reader.js
+++ routes/reader.js
@@ -14,7 +14,7 @@ router.route('/register').post((req, res) => {
     } = req.body;
 
     if (!first_name || !last_name || !email || !password) {
-
+        
         if(!first_name) {
             return res.status(400).json({
                 msg: 'First name is required.'
@@ -52,7 +52,7 @@ router.route('/register').post((req, res) => {
             return res.status(400).json({
                 msg: 'This email has been already used.'
             });
-            
+
         } else {
             bcrypt.genSalt(8, function(err, salt) {
                 if(err) {
@@ -78,7 +78,6 @@ router.route('/register').post((req, res) => {
                     return newReader.save()
                                 .then(() => res.json({msg: 'Account is created!'}))
                                 .catch(err => res.status(400).json('Error: ' + err));
-                        
                 });
             });
         }
@@ -86,27 +85,43 @@ router.route('/register').post((req, res) => {
     
 });
 
-/*router.route('/login').post((req, res) => {
-    const {email, password} = body.req;
+router.route('/login').post((req, res) => {
+    const {email, password} = req.body;
 
     if(!email || !password) {
+        if(!email) {
+            return res.status(400).json({msg: 'The Email is missing!'});
+        }
 
+        if(!password) {
+            return res.status(400).json({msg: 'The Password is missing!'});
+        }
     }
 
-    const reader = await Reader.findOne({ email: email });
+    Reader.findOne({ email: email }, (err, data) => {
+        if(err) {
+            return res.status(400).json({msg: 'Something went wrong! Please try again later.'});
+        }
 
-    if(!reader) {
+        if(data) {
+            bcrypt.compare(password, data.password, (err, result) => {
+                if(err) {
+                    return res.status(400).json({msg: 'Something went wrong! Please try again later.'})
+                }
 
-    }
+                if(result){
+                    jwt.sign({ id: data._id }, process.env.TOKEN_SECRET, (err, token) => {
+                        res.header('auth-token', token).send(token);
+                    });
 
-    const isMatch = await bcrypt.compare(password, reader.password);
-
-    if(!isMatch) {
-
-    } 
-
-    const token = jwt.sign({ id: reader._id });
-
-});*/
+                } else {
+                    return res.status(400).json({msg: 'Email or password wrong.'});
+                } 
+            });
+        } else {
+            return res.status(400).json({msg: 'Please create an account first!'});
+        }
+    });
+});
 
 module.exports = router;
