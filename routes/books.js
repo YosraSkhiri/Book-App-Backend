8448670857dd6600a4ac3e70aaa9eb08320103ab
Yosra <yos.skhiri@gmail.com>
2020-12-01 19:19:49 +0100

    added the search route for the books

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,28 @@
 const router = require('express').Router();
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+let path = require('path');
 let Book = require('../models/book.model');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function(req, file, cb) {   
+        cb(null, uuidv4() + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    let allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+    if(allowedFileTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+const upload = multer({ storage, fileFilter });
 
 router.route('/').get((req, res) => {
     Book.find()
@@ -7,11 +30,28 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
+
+router.route('/search').get((req, res) => {
+    const book = req.query.book;
+    let regex = new RegExp(book,'i');
+    return Book.find({
+        $or: [
+        {'title': regex}
+     ]
+    }).limit(4)
+      .then(books => res.json(books))
+      .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post(upload.single('cover'), (req, res) => {
     const title = req.body.title;
     const authors = req.body.authors;
     const description = req.body.description;
-    const cover = req.body.cover;
+    let cover = '';
+
+    if(req.file) {
+        cover = req.file.filename
+    }
 
     const newBookData = {
         title,
